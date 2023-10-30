7c189fccf2d8bf805be6e7a2fa93d8dfb4410f38
VilmaDediniene <vilmasestokaite1@gmail.com>
2023-10-30 21:45:27 +0200

    Add task Uzduotis8__1

---
+++ Uzduootis8__1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int num, product;
+            Console.Write("Įveskite skaičių ir atspausdinkite jo daugybos lentelę: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nSkaičiaus daugybos lentelė yra {0}: ", num);
+            for (int i = 1; i <= 10; i++)
+            {
+                product = num * i;
+                Console.WriteLine("{0} x {1} = {2}", num, i, product);
+            }
+
+
+            Console.ReadLine();
+            //Console.ReadKey();
+        }
+    }
+}
+

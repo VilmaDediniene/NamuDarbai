e47dd3fb94c243816400957052a82395f2576519
VilmaDediniene <vilmasestokaite1@gmail.com>
2023-10-30 21:42:34 +0200

    Add uzduotis_7

---
+++ Uzduotis_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Įveskite savo vardą: ");
+            string ivedimas = Console.ReadLine();
+            Console.Write("Įveskite savo amžių: ");
+            int amzius = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Jūsų vardas: " + ivedimas + ", o amžius " + amzius);
+            Console.ReadLine();
+        }
+    }
+}
